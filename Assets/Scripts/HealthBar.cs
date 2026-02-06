b28c20a6cf56ae079d4b63cdf29ca2aa06ecfa1a
Amhhhex <Amhhex@gmail.com>
2026-02-06 13:58:36 -0500

    finished readings

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -11,6 +11,15 @@ public class HealthBar : MonoBehaviour
 
     public int health;
 
+    public AudioSource AudioSource;
+
+    public AudioClip chomp;
+
+    public AudioClip death;
+
+    //AudioSource.Play() plays the current clip in audioSource.clip and stops/restarts it
+    //AudioSource.PlayOneShot(MyClip) plays MyClip and will play on top of a currently playing (on this Audio Source) clip
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -28,10 +37,16 @@ public class HealthBar : MonoBehaviour
         if(player.bounds.Contains(mousePos) && Mouse.current.leftButton.wasPressedThisFrame)
         {
             healthSlider.value-= 1;
+
+            AudioSource.clip = chomp;
+
+            AudioSource.Play();
         }
 
         if(healthSlider.value == 0)
         {
+            AudioSource.clip = death;
+            AudioSource.Play();
             gameObject.SetActive(false);
         }
         
@@ -41,6 +56,7 @@ public class HealthBar : MonoBehaviour
 
     public void heal()
     {
+        gameObject.SetActive(true);
         healthSlider.value += 5;
     }
 }
