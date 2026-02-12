439c06eb29a9d2382069e7d75532e1348a36f08f
Amhhhex <Amhhex@gmail.com>
2026-02-11 23:10:43 -0500

    Battery Health n stuff, also rotations

--- Assets/Scripts/Assignment2ThiefMovement.cs
+++ Assets/Scripts/Assignment2ThiefMovement.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Assignment2ThiefMovement : MonoBehaviour
 {
@@ -6,27 +7,58 @@ public class Assignment2ThiefMovement : MonoBehaviour
     public bool allowMovement;
     public GameObject artPiece;
     public float speed;
+
+    public Slider batteryHealth;
+
+    bool artPieceGrabbed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         allowMovement = true;
+        artPieceGrabbed = false;
+        batteryHealth.value = batteryHealth.maxValue;
         
     }
     // Update is called once per frame
     void Update()
     {
         Vector2 currentPosition = transform.position;
-        Vector2 artPiecePosition = artPiece.transform.position;
 
         if(allowMovement)
         {
             currentPosition.x -= speed * Time.deltaTime;
-            transform.position = currentPosition;   
+            transform.position = currentPosition;
+            
+            
         }
 
-        if(Vector2.Distance(currentPosition, artPiecePosition) < 1)
+        if(!allowMovement)
+        {
+            batteryHealth.value -= 5 * Time.deltaTime;
+        }
+
+        if(batteryHealth.value <= 0)
+        {
+            allowMovement = true;
+        }
+
+        if(Vector2.Distance(currentPosition, artPiece.transform.position) < 1 && !artPieceGrabbed)
         {
             speed *= -1;
+            artPieceGrabbed = true;
+            Vector2 rotatePosition = transform.eulerAngles;
+
+            rotatePosition.y += 180f;
+
+            transform.eulerAngles = rotatePosition;
+            
+        }
+
+        if(artPieceGrabbed)
+        {
+            
+            artPiece.transform.position = new Vector2(currentPosition.x + 0.5f, currentPosition.y + 0.5f);
+
         }
     }
 
