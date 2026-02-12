07014f6a2dabf67aedf0c0d977d67ef9b61afa3d
Amhhhex <Amhhex@gmail.com>
2026-02-11 21:40:24 -0500

    The thief walks up to the art piece and runs away

--- Assets/Scripts/Assignment2ThiefMovement.cs
+++ Assets/Scripts/Assignment2ThiefMovement.cs
@@ -4,7 +4,7 @@ public class Assignment2ThiefMovement : MonoBehaviour
 {
 
     public bool allowMovement;
-
+    public GameObject artPiece;
     public float speed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -12,23 +12,23 @@ public class Assignment2ThiefMovement : MonoBehaviour
         allowMovement = true;
         
     }
-
     // Update is called once per frame
     void Update()
     {
-
         Vector2 currentPosition = transform.position;
-
-
+        Vector2 artPiecePosition = artPiece.transform.position;
 
         if(allowMovement)
         {
             currentPosition.x -= speed * Time.deltaTime;
             transform.position = currentPosition;   
         }
-        
-    }
 
+        if(Vector2.Distance(currentPosition, artPiecePosition) < 1)
+        {
+            speed *= -1;
+        }
+    }
 
     public void lightSwitchOn()
     {
