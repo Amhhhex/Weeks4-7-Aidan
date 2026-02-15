538bf678b234433ffacc6a61fe973f48d0651f40
Amhhhex <Amhhex@gmail.com>
2026-02-15 17:46:56 -0500

    Commenting and code clean up

--- Assets/Scripts/Assignment2ThiefMovement.cs
+++ Assets/Scripts/Assignment2ThiefMovement.cs
@@ -3,37 +3,48 @@ using UnityEngine.UI;
 
 public class Assignment2ThiefMovement : MonoBehaviour
 {
-
+    //Bool values, the first enables movement while the second tracks when the painting has been grabbed
     public bool allowMovement;
-    public GameObject artPiece;
-    public float speed;
+    bool artPieceGrabbed;
 
+    //GameObject variables
+    //One is for the art piece while the second is for the darkLamp
+    public GameObject artPiece;
+    public GameObject darkLamp;
+
+    //Declaring a spriteRenderer, used to check if the thief overlaps with the painting
     SpriteRenderer thiefBounds;
 
-    public GameObject darkLamp;
     
-
+    //Declaration of slider variables
+    //Both of the slider values are stored in these public variables
     public Slider batteryHealth;
     public Slider thiefTimer;
 
+    //Declaration of some public float variables
+    //The first is used for the speed of the thief
+    //The second is used to decrease the battery health
+    public float speed;
     public float decreaseAmount = 1f;
 
-    bool artPieceGrabbed;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        //Setting the values of objects to there default/initial values
         allowMovement = true;
         artPieceGrabbed = false;
         batteryHealth.value = batteryHealth.maxValue;
 
+        //Using GetComponent to grab the spriteRenderer off of the thief gameObject
         thiefBounds = GetComponent<SpriteRenderer>();
 
+        //Setting the value of the thief timer slider to it's max value
         thiefTimer.value = thiefTimer.maxValue;
 
+        //Rotating the thief so that it is facing the correct direction
         Vector2 rotateImage = transform.eulerAngles;
-
         rotateImage.y += 180f;
-
         transform.eulerAngles = rotateImage;
         
         
@@ -41,68 +52,95 @@ public class Assignment2ThiefMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        //Storing the current position of the thief in a Vector2 variable
         Vector2 currentPosition = transform.position;
 
+
+        //Checking to see if the thief can move
+        //If it's true the thief moves
         if(allowMovement)
         {
+            //Move the thief by a specified amount
             currentPosition.x -= speed * Time.deltaTime;
             transform.position = currentPosition;
 
+            //Resetting the value of decreaseAmount to 1
             decreaseAmount = 1f;
 
+            //Setting the darkLamp gameObject to true, making it appear in the scene
             darkLamp.SetActive(true);
             
             
         }
 
+        //If allowMovement is false
         if(!allowMovement)
         {
+            //Deactivate the darkLamp game object
             darkLamp.SetActive(false);
+
+            //Start decreasing the value of the batteryHealth sliders value
             batteryHealth.value -= (5 + decreaseAmount) * Time.deltaTime;
+
+            //Increase decrease amount by a specified amount
+            //This makes the battery health slider decrease faster the longer it is being used
             decreaseAmount += 2f * Time.deltaTime;
         }
 
         
-
+        //If the slider has been depleted of it's value
         if(batteryHealth.value <= 0)
         {
+            //Set allowMovement to true, forcing the thief to move forward
             allowMovement = true;
+
+            //Activate the dark lamp game object
             darkLamp.SetActive(false);
         }
 
+        //Triggers when the spriterenderer's bounds overlaps with the artPiece's transform position
         if(thiefBounds.bounds.Contains(artPiece.transform.position) && !artPieceGrabbed)
         {
+            //Reverse the direction of the thief
             speed *= -1;
+            //Set artPieceGrabbed to true, making this only execute once
             artPieceGrabbed = true;
-            Vector3 rotatePosition = artPiece.transform.eulerAngles;
 
+            //Store the transform values of both the thief and the art piece
+            Vector3 rotatePosition = artPiece.transform.eulerAngles;
             Vector2 rotateThief = transform.eulerAngles;
 
+            //Rotate both of them
             rotateThief.y += 180f;
-
             transform.eulerAngles = rotateThief;
-
             rotatePosition.z += 90f;
-
             artPiece.transform.eulerAngles = rotatePosition;
             
         }
 
+        //If the art piece has been grabbed
         if(artPieceGrabbed)
         {
-            
+            //Make the artPiece follow the thief's transform position but with a slight offset
             artPiece.transform.position = new Vector2(currentPosition.x + 0.5f, currentPosition.y + 0.5f);
 
         }
 
+
+        //Decrease the thief Timer by the specified amount over time
         thiefTimer.value -= 5f * Time.deltaTime;
 
+        //If the thief timer has been depleted
         if(thiefTimer.value <= 0)
         {
+            //Destroy the thief
             Destroy(gameObject);
         }
     }
 
+    //This is a function used exclusively by the lightSwitch button
+    //This function toggles the value of allowMovement from true to false
     public void lightSwitchOn()
     {
         if (allowMovement)
--- Assets/Scripts/LampSpawner.cs
+++ Assets/Scripts/LampSpawner.cs
@@ -3,25 +3,23 @@ using UnityEngine.UI;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-
+    //The public variable for spawning the lamp prefab
     public GameObject lamp;
 
+    //An empty gameObject that will store the lamp object
     GameObject clone;
 
+    //A slider variable
     Slider slider;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
 
-        Vector3 objectPosition = transform.position;
-
-        objectPosition.y += 10;
-
-        transform.position = objectPosition;
-
+        //Instantiating the lamp object and storing the spawned object within clone
         clone = Instantiate(lamp, new Vector2(-6f, 3.5f), Quaternion.identity);
 
+        //retriving the slider object component from the battery health slider
         slider = GetComponent<Slider>();
 
         
@@ -31,8 +29,10 @@ public class NewBehaviourScript : MonoBehaviour
     void Update()
     {
 
+        //Once the slider reaches it's minimum value (0)
         if(slider.value == 0)
         {
+            //Destroy the lamp preFab
             Destroy(clone);
         }
         
